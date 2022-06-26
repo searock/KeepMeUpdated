fe1648058a7462aae2a5289034bfe6fb94d02d24
Searock Ruzario <searock@users.noreply.github.com>
2022-06-26 17:22:08 -0400

    Revert "another test"
    This reverts commit b787a2a1bb3688e77fc31d519de4cdb9e575cb69.

--- app/src/main/java/dev/searock/keepmeupdated/MainActivity.kt
+++ app/src/main/java/dev/searock/keepmeupdated/MainActivity.kt
@@ -4,9 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-    public var a = 2
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
